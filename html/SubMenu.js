8873d9983c8d6531d98abbea3cf590c66656d878
BlackPing <47684708+BlackPing@users.noreply.github.com>
2019-04-22 09:40:10 +0900

    2019-04-22 relese

---
+++ html/SubMenu.js
@@ -0,0 +1,9 @@
+var subList = ['공사중'];
+for(var i = 0; i < subList.length; i++)
+{
+  var val = i + 1;
+  if(smSelect == i)
+    document.write('<a style="color:black; text-decoration:none;" href="'+ val +'.html"><li style="color:black; background:white; border-color="black"">'+subList[i]+'</li></a>');
+  else
+    document.write('<a style="color:white; text-decoration:none;" href="'+ val +'.html"><li>'+subList[i]+'</li></a>');
+}
